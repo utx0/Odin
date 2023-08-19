baa82d1321eca9a4f797e882bc7fe985b627939e
LUKE™ <utx0@mettadata.com>
2023-08-19 21:31:58 +1000

    renamed the thorchain package

--- odin/src/main.rs
+++ odin/src/main.rs
@@ -3,22 +3,25 @@ extern crate dotenv;
 use dotenv::dotenv;
 use std::env;
 
-const NODE_URI: &'static str = "/thorchain/nodes";
-const VAULTS_URI: &'static str = "/thorchain/vaults/asgard";
+// const NODE_URI: &'static str = "/thorchain/nodes";
+// const VAULTS_URI: &'static str = "/thorchain/vaults/asgard";
 
-fn main() {
+#[tokio::main]
+async fn main() {
     dotenv().ok();
 
-    let node = env::var("NODE").expect("NODE must be set");
+    // let node = env::var("NODE").expect("NODE must be set");
 
-    println!("NODE_URL: {}{}", node, NODE_URI);
-    println!("VAULTS_URL: {}{}", node, VAULTS_URI);
+    // println!("NODE_URL: {}{}", node, NODE_URI);
+    // println!("VAULTS_URL: {}{}", node, VAULTS_URI);
 
-    let node_url = format!("{}{}", node, NODE_URI);
-    let vaults_url = format!("{}{}", node, VAULTS_URI);
+    // let node_url = format!("{}{}", node, NODE_URI);
+    // let vaults_url = format!("{}{}", node, VAULTS_URI);
 
-    let resp = reqwest::blocking::get(&node_url).unwrap();
-    let vaults = reqwest::blocking::get(&vaults_url).unwrap();
+    // let resp = reqwest::blocking::get(&node_url).unwrap();
+    // let vaults = reqwest::blocking::get(&vaults_url).unwrap();
 
-    println!("NODE: {}", resp.text().unwrap());
+    // println!("NODE: {}", resp.text().unwrap());
+
+
 }
