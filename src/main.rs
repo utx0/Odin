b33fd13da65c72ad4e848336b50a7ba7068b0914
LUKE™ <utx0@mettadata.com>
2023-08-19 18:11:43 +1000

    dotenv and static uri's

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+extern crate dotenv;
+
+use dotenv::dotenv;
+use std::env;
+
+const NODE_URI: &'static str = "/thorchain/nodes";
+const VAULTS_URI: &'static str = "/thorchain/vaults/asgard";
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+
+    let node = env::var("NODE").expect("NODE must be set");
+
+    println!("NODE_URL: {}{}", node, NODE_URI); 
+    println!("VAULTS_URL: {}{}", node, VAULTS_URI);
 }
