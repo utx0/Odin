92d797235c5365513ccbb295e9c0f9ef8fdd5ac6
LUKE™ <utx0@mettadata.com>
2023-09-03 23:04:35 +1000

    feat: added spawn churn checks

--- odin/src/main.rs
+++ odin/src/main.rs
@@ -1,8 +1,6 @@
 use state::State;
 use std::sync::Arc;
-use thorchain_rs::apis::configuration::Configuration;
-use thorchain_rs::apis::vaults_api::asgard;
-use thorchain_rs::apis::vaults_api::AsgardParams;
+use tokio::sync::oneshot;
 use tokio::sync::Mutex;
 
 mod state;
@@ -11,20 +9,24 @@ mod state;
 async fn main() {
     let state = Arc::new(Mutex::new(state::State::new()));
 
-    check_vaults(state.clone()).await;
+    let (_tx, rx) = oneshot::channel::<()>();
+
+    spawn_churn_checks(state.clone()).await;
+
+    println!("Obin is watching...");
+
+    let _ = rx.await;
 }
 
-async fn check_vaults(state: Arc<Mutex<State>>) {
-    loop {
-        let state = state.clone();
-        let task = tokio::spawn(async move {
+async fn spawn_churn_checks(state: Arc<Mutex<State>>) -> tokio::task::JoinHandle<()> {
+    tokio::spawn(async move {
+        loop {
             let mut state = state.lock().await;
+            state.update_churn_interval().await;
             state.update_vaults().await;
-            println!("{:?}", state.print_vaults());
-        });
-
-        task.await.unwrap();
-
-        tokio::time::sleep(tokio::time::Duration::from_secs(1)).await; //TODO create a smarter way to time the loop
-    }
+            let seconds = state.seconds_to_churn().await;
+            println!("{} seconds before next churn checks", seconds);
+            tokio::time::sleep(tokio::time::Duration::from_secs(seconds)).await;
+        }
+    })
 }
--- odin/src/state.rs
+++ odin/src/state.rs
@@ -3,11 +3,11 @@ use thorchain_rs::apis::mimir_api;
 use thorchain_rs::apis::mimir_api::MimirKeyParams;
 use thorchain_rs::apis::vaults_api::{asgard, AsgardParams};
 
-const CHURN_MAX_RANGE: i64 = 43200;
+const CHURN_MAX_RANGE: u64 = 43200;
+const AVG_BLOCKSPEED: u64 = 6;
 
 pub struct State {
-    churn_interval: Option<i64>,
-    // vaults: Vec<Vault::vault>,
+    churn_interval: Option<u64>,
     vaults: Vec<thorchain_rs::models::vault::Vault>,
     config: Configuration,
 }
@@ -29,10 +29,10 @@ impl State {
 
         let churn_interval = mimir_api::mimir_key(&self.config, params).await.unwrap();
 
-        self.churn_interval = Some(churn_interval);
+        self.churn_interval = Some(churn_interval as u64);
     }
 
-    pub fn churn_interval(&self) -> Option<i64> {
+    pub fn churn_interval(&self) -> Option<u64> {
         self.churn_interval
     }
 
@@ -50,5 +50,10 @@ impl State {
 
     pub fn print_vaults(&self) {
         println!("{:#?}", self.vaults);
+    }
+
+    pub async fn seconds_to_churn(&self) -> u64 {
+        // TODO add check for above upper range. ie churn paused.
+        (self.churn_interval.unwrap() * AVG_BLOCKSPEED) / 2
     }
 }
