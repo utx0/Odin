d35b27e634162df4ceb4aa1cf04ed749d3d71c76
LUKE™ <utx0@mettadata.com>
2023-09-03 22:16:40 +1000

    wip working in threads, but creates tasks each time. Needs to be refactored

--- odin/src/main.rs
+++ odin/src/main.rs
@@ -1,15 +1,30 @@
 use state::State;
+use std::sync::Arc;
 use thorchain_rs::apis::configuration::Configuration;
 use thorchain_rs::apis::vaults_api::asgard;
 use thorchain_rs::apis::vaults_api::AsgardParams;
+use tokio::sync::Mutex;
 
 mod state;
 
 #[tokio::main]
 async fn main() {
-    let mut state = state::State::new();
+    let state = Arc::new(Mutex::new(state::State::new()));
 
-    state.update_vaults().await;
+    check_vaults(state.clone()).await;
+}
 
-    println!("{:?}", state.print_vaults()); 
+async fn check_vaults(state: Arc<Mutex<State>>) {
+    loop {
+        let state = state.clone();
+        let task = tokio::spawn(async move {
+            let mut state = state.lock().await;
+            state.update_vaults().await;
+            println!("{:?}", state.print_vaults());
+        });
+
+        task.await.unwrap();
+
+        tokio::time::sleep(tokio::time::Duration::from_secs(1)).await; //TODO create a smarter way to time the loop
+    }
 }
